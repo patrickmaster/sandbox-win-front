bd80789e0a9a367ea58c79af18f31730113041f2
patryk <patrykbuzowicz@gmail.com>
2015-05-31 17:48:17 +0200

    code page changes

--- app/scripts/controllers/code.js
+++ app/scripts/controllers/code.js
@@ -14,33 +14,53 @@ angular.module('sandboxApp').controller('CodeCtrl', function ($scope, platforms,
     $scope.code = null;
     $scope.executionResult = null;
     $scope.executionStatus = null;
+    $scope.selectedPlatform = $scope.platforms[0];
+    $scope.waitingForResult = false;
+    $scope.useCodeWrapper = true;
 
     $scope.requestExecution = function () {
+        $scope.waitingForResult = true;
         operation.post({
-            Platform: $scope.selectedPlatform.Id,
-            ReturnType: 1,
-            Code: $scope.code
+            Platform: $scope.selectedPlatform.ID,
+            Libraries: getLibraries(),
+            Code: $scope.code,
+            UseWrapper: $scope.useCodeWrapper
         }, function (data) {
-            pullResult(data.Id);
+            pullResult(data.ID);
         });
     };
-    
+
+    function getLibraries() {
+        var libs = [];
+        for (var i = 0; i < $scope.libraries.length; i++) {
+            if ($scope.libraries[i].selected && $scope.libraries[i].Platform === $scope.selectedPlatform.ID) {
+                libs.push($scope.libraries[i]);
+            }
+        }
+
+        return libs;
+    }
+
     function pullResult(id) {
         $timeout(function () {
             operation.get({id: id}, function (data) {
-                if (typeof data.Result !== 'undefined' || typeof data.Exception !== 'undefined') {
+                if (typeof data.Result !== 'undefined' || typeof data.Error !== 'undefined') {
                     putResult(data);
+                    $scope.waitingForResult = false;
                 }
                 else {
                     pullResult(id);
                 }
+            }, function () {
+                $scope.waitingForResult = false;
+                putResult({Error: 'Nieoczekiwany błąd'});
             });
         }, 500);
     }
 
     function putResult(data) {
-        if (data.Exception) {
-            $scope.executionResult = data.Exception;
+        if (data.Error) {
+            $scope.executionResult = data.Error;
             $scope.executionStatus = 'execution-failure';
         }
         else {
@@ -48,7 +68,5 @@ angular.module('sandboxApp').controller('CodeCtrl', function ($scope, platforms,
             $scope.executionStatus = 'execution-success';
         }
     }
-
-    $scope.selectedPlatform = $scope.platforms[0];
 
 });
